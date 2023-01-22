b9c48d268ac2f9e7a673b2c90040851efb33c201
HMLTFan <xfqwdsj@qq.com>
2023-01-22 07:26:24 +0000

    fix script

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,8 @@
 $component = ([string]$app).Substring(12)
 $target_dir = "$(persistdir android-sdk-base $global)\$component"
 
-attrib -r "$target_dir\*" /l
-Remove-Item -Path "$target_dir\*" -Recurse -Force
+Get-ChildItem "$target_dir\*" | ForEach-Object {
+    attrib -r $_.FullName /l
+}
+
+Remove-Item -Path $target_dir\* -Recurse -Force
