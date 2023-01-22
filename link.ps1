af19a6055fa9686ebf412ea131923c37be9021b8
HMLTFan <xfqwdsj@qq.com>
2023-01-22 06:51:35 +0000

    update

--- link.ps1
+++ link.ps1
@@ -1,10 +1,12 @@
+. setup.ps1
+
 Get-ChildItem -Path "$dir\$component" | ForEach-Object {
     $source = "$target_dir\$($_.Name)"
 
     if ($_.PSIsContainer) {
-        New-Item -Path $source -ItemType Junction -Value $_
-        attrib $source +r /l
+        New-Item -Path $source -ItemType Junction -Value $_ -Force
+        attrib +r $source /l
     } else {
-        New-Item -Path $source -ItemType HardLink -Value $_
+        New-Item -Path $source -ItemType HardLink -Value $_ -Force
     }
 }
--- setup.ps1
+++ setup.ps1
@@ -1,10 +1,5 @@
 $component = ([string]$app).Substring(12)
 $target_dir = "$(persistdir android-sdk-base $global)\$component"
 
-Get-ChildItem -Path "$target_dir\*" | ForEach-Object {
-    if ($_.Attributes -band [IO.FileAttributes]::ReparsePoint) {
-        $_.Delete()
-    } else {
-        Remove-Item -Path $_ -Recurse -Force
-    }
-}
+attrib -r "$target_dir\*" /l
+Remove-Item -Path "$target_dir\*" -Recurse -Force
